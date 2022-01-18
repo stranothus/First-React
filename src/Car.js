693cd312809b1d08fb1fdbd235ca9089a5399edf
stranothus <stranothus@gmail.com>
2022-01-18 15:59:05 -0600

    Set color state to red when clicked

--- src/Car.js
+++ src/Car.js
@@ -9,10 +9,16 @@ class Car extends React.Component {
             color: "#000000"
         };
     }
+    redColor = () => {
+        this.setState({ color: "#FF0000" });
+    }
+    blackColor = () => {
+        this.setState({ color: "#000000" });
+    }
     render() {
         return (
             <>
-                <h2>I am a car</h2>
+                <h2 onClick={() => this.state.color === "#000000" ? this.redColor() : this.blackColor()} style={{color: this.state.color}}>I am a car</h2>
                 {this.state.zoom ? <h3>Vroom vroom</h3> : ""}
             </>
         );
